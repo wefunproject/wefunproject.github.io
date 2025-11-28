e168f8fad27f7c45b8480706402fb18b157c8fa1
Lucas <alonsolucas.developer@gmail.com>
2025-11-28 11:36:12 +0100

    Commented the console.log

--- scripts/common.js
+++ scripts/common.js
@@ -63,7 +63,7 @@ function cargarHeaderYFooter() {
     ? '/components/header-index.html'
     : '/components/my-header.html';
 
-  console.log("Cargando header desde:", headerUrl);
+  // console.log("Cargando header desde:", headerUrl);
 
   const headerPromise = fetch(headerUrl)
     .then(response => response.text())
